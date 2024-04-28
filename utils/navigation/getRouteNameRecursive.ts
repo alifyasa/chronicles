699cf7df0be8cd87602ada601f586fdfd6c7c53b
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-04-28 19:40:12 +0700

    feat: Better nav logging

--- utils/navigation/getRouteNameRecursive.ts
+++ utils/navigation/getRouteNameRecursive.ts
@@ -5,7 +5,10 @@ interface Route {
   };
 }
 
-const getRouteNamesRecursive = (routes: Route[], parent: string): string[] => {
+const getRouteNamesRecursive = (
+  routes: Route[],
+  parent: string = ""
+): string[] => {
   return routes.flatMap((route: Route) => {
     const currentName = `${parent}/${route.name}`;
     if (!route.state) {
